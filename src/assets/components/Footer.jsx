0f26c8a2d6e560a228cc4c1d23a71e0508279d07
Kriangkai <maylonpan@gmail.com>
2025-06-18 00:57:48 +0700

    Finish with Home responsive

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import { gsap } from "gsap";
+import { Link } from 'react-router-dom';
 
 gsap.from("h3,p,input", {
-    y: 100, // เลื่อนจาก 100px ด้านล่าง
-    opacity: 0, // เริ่มจาก opacity 0
-    duration: 1, // ใช้เวลา 1 วินาที
-    delay: 0.5, // หน่วงเวลา 0.5 วินาที
-    ease: "power2.out" // ชนิดของ easing
+    y: 100,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+    ease: "power2.out"
 });
 
 function Footer() {
   return (
-    <div className='max-w-[1200px] min-w-screen flex flex-col md:flex-row mt-10'>
-        <div className='ml-[20px]'>
+    <div className=' flex flex-col md:flex-row mt-10 pb-10 pl-10'>
+        <div className='mb-5'>
           <h3 className='mb-2'>NEWSLETTER</h3>
           <p className='mb-5'>Subscribe to receive updates, access to exclusive deal and more</p>
           <h3>Email:</h3>
-          <input type="text" className='border-b-1 w-[500px] mt-5'/>
+          <input type="text" className='border-b-1 w-2/3 md:w-full mt-5'/>
         </div>
-        <div className='ml-[20px] mt-[20px] md:mt-0 md:ml-0 md:mr-[20px]'>
-          <h3>hello</h3>
+        <div className='flex md:justify-end md:pr-20 w-full mt-[20px] '>
+          <div className='flex flex-col gap-5'>
+            <Link to='/' className='animated-underline'>HOME</Link>
+            <Link to='/about' className='animated-underline'>ABOUT</Link>
+            <Link to='/project' className='animated-underline'>PROJECTS</Link>
+            <Link to='/contact' className='animated-underline'>CONTACTS</Link>
+          </div>
         </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { CircleArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { gsap } from "gsap";
 
-gsap.from("h3,h4,h5,h6,p,input,span,img", {
+gsap.from("h3,h4,h5,h6,p,input,span,img,Link", {
     y: 100,
     opacity: 0,
     duration: 1,
@@ -17,32 +17,55 @@ gsap.from("h3,h4,h5,h6,p,input,span,img", {
 function Home() {
   return (
     <div>
-      <div>
+      <div className='max-w-[1440px] mx-auto'>
         <Navbar />
         {/* Head */}
         <div>
-          <div className='relative'>
+          <div className='relative flex md:hidden'>
             <img src="/astra-sky-river/astra-sr1.jpg" id='headerImg' className='w-screen'/>
             <div className='absolute bottom-0 pl-5'>
               <div className='flex'>
-                <h5 className='text-[3rem]'>PROFESSIONAL ADVICE</h5>
+                <h5 className='text-[2rem]'>PROFESSIONAL ADVICE</h5>
               </div>
               <div>
-                <h6 className='text-[3rem]'>FROM OUR DESIGNER</h6>
-                <p className='text-[1.5rem] my-8'>Our studio guarantees comfortable cooperation and the development of modern concepts for your home</p>
-                <CircleArrowRight className='md:hidden size-18 hover:cursor-pointer'/>
+                <h6 className='text-[2rem]'>FROM OUR DESIGNER</h6>
+                <p className='text-[1rem] my-8'>Our studio guarantees comfortable cooperation and the development of modern concepts for your home</p>
+                {/* <CircleArrowRight className='md:hidden size-10 hover:cursor-pointer'/> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='hidden md:flex  relative'>
+          <div className='w-2/3'>
+            <img src="/astra-sky-river/astra-sr1.jpg" id='headerImg' className=''/>
+            <div className='absolute bottom-0'>
+              <p className='text-[3rem]'>PROFESSIONAL ADVICE</p>
+              <div className='flex my-8'>
+                <p className='w-80 pl-10 flex items-center'>Our studio guarantees comfortable cooperation and the development of modern concepts for your home</p>
+                <p className='ml-30 text-[3rem] flex justify-end'>FROM OUR DESIGNER</p>
               </div>
             </div>
           </div>
         </div>
         {/* About */}
-        <div className='mt-20'>
-          <h5 className='text-[3rem] mb-5 pl-5'>ABOUT US</h5>
-          <div>
-            <p className='mb-5 pl-5 text-[1.25rem]'>Our studio specializes in creating concepts of modern interior solutions and their subsequent implementation.</p>
-            <p className='pl-5 text-[1.25rem]'>We have a lot of experience, and it helps to easily solve any issues and keep abreast of trends.</p>
+        <div className='mt-20 md:flex'>
+          <div className='md:w-1/3'>
+            <p className='text-[3rem] mb-5 pl-5 md:text-[3rem]'>ABOUT US</p>
+            <p className='hidden md:flex mb-5 pl-5 text-[1.25rem]'>Our studio specializes in creating concepts of modern interior solutions and their subsequent implementation.</p>
+            <p className='hidden md:flex pl-5 text-[1.25rem]'>We have a lot of experience, and it helps to easily solve any issues and keep abreast of trends.</p>
+            <div className='md:flex items-center h-20 mt-5 gap-5 pl-5 hidden'>
+              <p className='text-[1.5rem]'>LEARN MORE</p>
+              <Link to='/about'>
+                <CircleArrowRight className='size-10 hover:cursor-pointer'/>
+              </Link>
+            </div>
+          </div>
+          <div className='md:w-2/3'>
+            <p className='mb-5 pl-5 text-[1.25rem] md:hidden'>Our studio specializes in creating concepts of modern interior solutions and their subsequent implementation.</p>
+            <p className='pl-5 text-[1.25rem] md:hidden'>We have a lot of experience, and it helps to easily solve any issues and keep abreast of trends.</p>
             <img src="/astra-sky-river/astra-sr3.jpg" className='mt-5 w-screen'/>
-            <div className='flex items-center h-20 mt-5 gap-5 pl-5'>
+            <div className='flex items-center h-20 mt-5 gap-5 pl-5 md:hidden'>
               <p className='text-[1.5rem]'>LEARN MORE</p>
               <Link to='/about'>
                 <CircleArrowRight className='size-10 hover:cursor-pointer'/>
@@ -52,7 +75,7 @@ function Home() {
         </div>
         {/* Stats */}
         <div className='mt-20'>
-          <div className='grid grid-cols-2 pl-5 gap-10'>
+          <div className='grid grid-cols-2 pl-5 gap-10 md:flex md:justify-between'>
             <div className='w-40'>
               <p id='stat'>10+</p>
               <p id='statDesc'>years of work <br/> of our agancy</p>
@@ -76,19 +99,19 @@ function Home() {
         <div className='mt-20 pl-5'>
           <div className='mb-5'>
             <h3 className='text-[2rem]'>OUR</h3>
-            <h5 className='text-[3rem]'>SERVICES</h5>
+            <p className='text-[3rem]'>SERVICES</p>
           </div>
-          <div className=''>
+          <div className='md:flex md:justify-between md:gap-10'>
             <div className='mb-5'>
-              <p className='text-[1.5rem]'>Residential design</p>
+              <p className='text-[1.5rem] font-semibold mb-3'>Residential design</p>
               <p>Our design studio is ready to help develop a unique, modern and convenient project for your apartment, taking into account your desires, ideas and views</p>
             </div>
             <div className='mb-5'>
-              <p className='text-[1.5rem]'>Office design</p>
+              <p className='text-[1.5rem] font-semibold mb-3'>Office design</p>
               <p>The main task of the studio is to make the workspace harmonious and comfortable. It is this kind of environment that encourages employees to work productively</p>
             </div>
             <div className='mb-5'>
-              <p className='text-[1.5rem]'>Commercial space design</p>
+              <p className='text-[1.5rem] font-semibold mb-3'>Commercial space design</p>
               <p>Often people like to return to those places where they liked not only the service, but also the pleasant atmosphere of the room, which caused emotions</p>
             </div>
           </div>
@@ -97,7 +120,7 @@ function Home() {
         <div className='mt-20 pl-5'>
           <div>
             <h3>WHAT IS</h3>
-            <h5>OUR WORK</h5>
+            <p className='text-[3rem]'>OUR WORK</p>
           </div>
           <img src="/arise-charoenmuang/arise-crm2.jpg"/>
           <div className='mt-5 flex flex-col gap-5'>
@@ -123,7 +146,7 @@ function Home() {
         <div className='mt-20 pl-5 pr-5'>
           <div>
             <h3>OUR</h3>
-            <h5>COMPLETED PROJECTS</h5>
+            <p className='text-[3rem]'>COMPLETED PROJECTS</p>
           </div>
           <div className='mt-10'>
             <ProjectCard
